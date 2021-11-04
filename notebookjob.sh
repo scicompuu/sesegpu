416e95e55e075d12495efe06ab1b351165d67d36
Martin Kronbichler <martin.kronbichler@it.uu.se>
2021-11-04 14:27:42 +0100

    Update some more paths

--- notebookjob.sh
+++ notebookjob.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#SBATCH -A g2020014 -t 0:59:00 -p core -n 4 -M snowy --gres=gpu:t4:1 --parsable
+#SBATCH -A g2021027 -t 0:59:00 -p core -n 4 -M snowy --gres=gpu:t4:1 --parsable
 CENV=${5-tf-gpu}
 CONTAINER=${6-container.sif}
 singularity run --nv /proj/g2020014/nobackup/private/$CONTAINER -c "conda activate $CENV && jupyter lab --ip=127.0.0.1 --port=$1" && exit
--- runnogpu.sh
+++ runnogpu.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-#SBATCH -A g2020014 -t 0:59:00 -p devcore -n 4 -M snowy --parsable
+#SBATCH -A g2021027 -t 0:59:00 -p devcore -n 4 -M snowy --parsable
 singularity run /proj/g2020014/nobackup/private/$@
